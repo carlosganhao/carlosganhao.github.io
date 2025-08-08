9e5bf8deffd6f2038a89b5f413ef43f5c48fb2c4
carlosganhao <cooli2.cooli2@hotmail.com>
2025-08-08 16:52:38 +0100

    wip

---
+++ src/components/Hero.svelte
@@ -0,0 +1,12 @@
+<script>
+    import ItchLogo from "../icons/ItchLogo.svelte";
+</script>
+
+<div class="mb-16">
+    <h1 class="text-8xl">Cooli2</h1>
+    <div class="flex">
+        <a class="text-white">
+            <ItchLogo />
+        </a>
+    </div>
+</div>
